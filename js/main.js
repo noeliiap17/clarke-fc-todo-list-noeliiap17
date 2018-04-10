eb0a951165fe46e77eeb998a35147c51e80e487b
U-DESKTOP-7348983\Noelia <you@example.com>
2018-04-10 15:48:12 +0200

    styles and first functions

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const newHomeworkContainer = document.querySelector('.newHomerwork__container');
+const moreHomeworkButton = document.querySelector('.button__add');
+const addHomeworkButton = document.querySelector('.newHomerwork__button');
+const homeworksContainer = document.querySelector('.list__chores');
+function showNewHomeworkContainer() {
+  newHomeworkContainer.classList.toggle('hidden');
+}
+function paintHomework() {
+  const newHomework = document.querySelector('.newHomerwork__input').value;
+  var homework = '<li class="item"><input type="checkbox" class="checkbox"><h1>'+ newHomework +'</h1></li>';
+  homeworksContainer.innerHTML += homework;
+  console.log(newHomework)
+
+}
+moreHomeworkButton.addEventListener('click', showNewHomeworkContainer);
+addHomeworkButton.addEventListener('click', paintHomework);
